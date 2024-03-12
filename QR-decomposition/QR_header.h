34089ce2a2d3ec911a29de9172b7e75b253163e8
ARBUZNIJ <120045631+ARBUZNIJ@users.noreply.github.com>
2024-03-12 15:13:18 +0300

    Add files via upload

--- QR-decomposition/QR_header.h
+++ QR-decomposition/QR_header.h
@@ -1,10 +1,10 @@
-#pragma once
+п»ї#pragma once
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
 #include <omp.h>
 #include <chrono>
-//#include <C:\Program Files (x86)\Intel\oneAPI\mkl\2023.2.0\include\mkl.h>
+#include <mkl.h>
 
 //const size_t bs = 32;
 using namespace std;
@@ -14,23 +14,21 @@ class QR
 private:
 
 	size_t i, j, k, n, m, block_size;
-	T* Q, * R, * REF_A, * v, * u, * factor_block,  * factor, *w, *z, * R_tmp;
-	T eps = 1e-10 /*, gamma*/;
+	T* Q, * R, * REF_A, * u, * factor_block, * w, * z, * R_tmp, * v_vector;
+	T norm, eps = 1e-10;
 
 public:
 
-	QR(bool flag, size_t size, size_t b_s) : n(size), block_size(b_s)//flag = 0 - ввод рандомных чисел, иначе ввод с клавиатуры
+	QR(bool flag, size_t size, size_t b_s) : n(size), block_size(b_s) //flag = 0 - ГўГўГ®Г¤ Г°Г Г­Г¤Г®Г¬Г­Г»Гµ Г·ГЁГ±ГҐГ«, ГЁГ­Г Г·ГҐ ГўГўГ®Г¤ Г± ГЄГ«Г ГўГЁГ ГІГіГ°Г»
 	{
-		Q = new T[n * n]();
-		R = new T[n * n];
-		R_tmp = new T[n * n];
 		REF_A = new T[n * n];
+		Q = new T[n * n]();
+		R = new T[n * (n+1)];
+		R_tmp = new T[n * n];
 		u = new T[n];
-		v = new T[block_size * n];
-		factor_block = new T[block_size];
-		factor = new T[block_size * n];
-		w = new T[block_size * n]();
-		z = new T[n];
+		v_vector = new T[n];
+		factor_block = new T[n];
+		w = new T[n * n]();
 
 		if (flag)
 		{
@@ -56,16 +54,15 @@ public:
 			for (i = 0; i < n; i++)
 				R[i * n + i] += n;
 
-			//#pragma omp parallel for schedule(static) private(i)
 			for (i = 0; i < n; i++)
 				copy(R + i * n, R + (i + 1) * n, REF_A + i * n);
 
-			/*auto start{ chrono::steady_clock::now() };
-			LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, n, n, REF_A, n, v);
-			LAPACKE_dorgqr(LAPACK_ROW_MAJOR, n, n, n, REF_A, n, v);
+			auto start{ chrono::steady_clock::now() };
+			LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, n, n, REF_A, n, v_vector);
+			LAPACKE_dorgqr(LAPACK_ROW_MAJOR, n, n, n, REF_A, n, v_vector);
 			auto end{ chrono::steady_clock::now() };
 			chrono::duration<double> elapsed_seconds = end - start;
-			cout << "Time spent (MKL): " << elapsed_seconds.count() << endl;*/
+			cout << "Time spent (MKL): " << elapsed_seconds.count() << endl;
 
 			for (i = 0; i < n; i++)
 				copy(R + i * n, R + (i + 1) * n, REF_A + i * n);
@@ -84,7 +81,7 @@ public:
 		return v;
 	}
 
-	void count_v_gamma(size_t column, size_t num_in_block)
+	void count_v_gamma(size_t column)
 	{
 		T scl, gamma;
 		//#pragma omp parallel for simd reduction(+: scl) 
@@ -98,7 +95,7 @@ public:
 
 		if (scl < eps)
 		{
-			v[num_in_block * n + column] = 1;
+			v_vector[column] = 1;
 			gamma = 0.5;
 			return;
 		}
@@ -108,27 +105,27 @@ public:
 			scl = 1 / sqrt(scl);
 			u[0] *= scl;
 			gamma = (1 + abs(u[0]));
-			v[num_in_block * n + column] = sgn(u[0]) * gamma;
+			v_vector[column] = sgn(u[0]) * gamma;
 
 			//#pragma omp parallel for simd
 			for (size_t i = column + 1; i < n; i++)
-				v[num_in_block * n + i] = u[i - column] * scl;
+				v_vector[i] = u[i - column] * scl;
 		}
 	}
-	
-	T sgn(T val)                //неканоничный sign(x), который возвращает 1 для неотрицательных x, иначе -1
+
+	T sgn(T val)
 	{
 		if (val >= 0)
 			return 1;
 		else return -1;
 	}
 
-	T scal(size_t v_num_in_block, size_t v_ind, size_t a_col)  //функция скалярного произведения для k столбцa матрицы A и вектора v, первый ненулевой элемент в котором располагается в позиции ind
+	T scal(size_t v_ind, size_t a_col) 
 	{
 		T res = 0;
-//#pragma omp simd
+		//#pragma omp simd
 		for (size_t i = v_ind; i < n; i++)
-			res += R[i * n + a_col] * v[v_num_in_block * n + i];
+			res += R[i * n + a_col] * v_vector[i];
 		return res;
 	}
 	void HHolder_R()
@@ -136,140 +133,199 @@ public:
 		size_t m = 0;
 
 		for (; m < (n / block_size); m++)
-		
-			HHolder_Block(m * block_size, block_size);
+
+			HHolder_Block(m * block_size);
 
 		if (n % block_size != 0)
-			HHolder_Block(m * block_size, n - m * block_size);
+			HHolder_Block_finish(m * block_size, n - m * block_size);
 	}
-	void HHolder_Block(size_t i_start, size_t b_size)
+	void HHolder_Block(size_t i_start)
 	{
 		size_t num_in_block;
 
-		for (i = 0; i < block_size; i++)
-			memset(v + i * n, 0, n * sizeof(T));
+		for (j = i_start; j < i_start + block_size; j++)
+		{
+			num_in_block = j - i_start;
+			count_v_gamma(j);
+
+			if (n - i_start >= 512)
+			{
+#pragma  omp parallel for private (k) //512/64 slowdown, 1024/64 minor boost (simd in scal - slowdown)
+				for (k = j; k < i_start + block_size; k++)
+					factor_block[k - j] = scal(j, k) / abs(v_vector[j]);
+			}
+			else
+				for (k = j; k < i_start + block_size; k++)
+				factor_block[k - j] = scal(j, k) / abs(v_vector[j]);
+
+			if (n - i_start >= 512)
+			{
+#pragma omp parallel for simd private (i) //512/64 slowdown, 1024/64 minor boost
+				for (i = j; i < n; i++)
+					for (k = j; k < i_start + block_size; k++)
+						R[i * n + k] -= v_vector[i] * factor_block[k - j];
+			}
+			else for (i = j; i < n; i++)
+				for (k = j; k < i_start + block_size; k++)
+					R[i * n + k] -= v_vector[i] * factor_block[k - j];
+
+			for (i = j; i < n; i++)
+				R[(i + 1) * n + j] = v_vector[i];
+	
+		}
+		
+		W_count(i_start);
+		
+		Q_count(i_start);
+
+		R_recount(i_start);
+	}
+
+	void HHolder_Block_finish(size_t i_start, size_t b_size)
+	{
+		size_t num_in_block;
 
 		for (j = i_start; j < i_start + b_size; j++)
 		{
 			num_in_block = j - i_start;
-			count_v_gamma(j, num_in_block); //вычислили vi и заполнили ими v[]
+			count_v_gamma(j);
 
 			for (k = j; k < i_start + b_size; k++)
-				factor_block[k - j] = scal(num_in_block, j, k) / abs(v[num_in_block * n + j]); //вычислили к-ты для vi в блоке
+				factor_block[k - j] = scal(j, k) / abs(v_vector[j]);
 
 			for (i = j; i < n; i++)
-			{
 				for (k = j; k < i_start + b_size; k++)
-					R[i * n + k] -= v[num_in_block * n + i] * factor_block[k - j]; //повычитали из блока
-			}
+					R[i * n + k] -= v_vector[i] * factor_block[k - j];
 		}
+	}
 
-		for (i = 0; i < block_size; i++)
-			memset(w + i * n, 0, n * sizeof(T));
-
-		for (i = 0; i < n; i++)
-		{
-			w[i * block_size] = (-2) * v[i];
-		}
-
-		for (i = 1; i < block_size; i++) //по столбцам результата в w
-		{
-			for (j = 0; j < n; j++) //по строкам W
-			{
-				for (k = 0; k < n; k++) //по строкам Y == v
-				{
-					for (m = 0; m < i/*block_size*/; m++) //по столбцам W
-					{
-
-						w[j * block_size + i] += (w[j * block_size + m] * v[m * n + k] + (j == k ? 1 : 0)) * v[i * n + k] * (-2);
-					}
-				}
-			}
-		}
-
-		for (i = 0; i < n; i++)
-			memset(Q + i * n, 0, n * sizeof(T));
-
-		for (j = 0; j < n; j++) //по строкам W
-		{
-			for (k = 0; k < n; k++) //по строкам Y == v
-			{
-				for (m = 0; m < block_size; m++) //по столбцам W
-				{
-					Q[j * n + k] += v[m * n + j] * w[k * block_size + m];
-				}
-			}
-		}
-
-		for (j = 0; j < n; j++)
-			Q[j * n + j] += 1;
-
-	/*	for (i = 0; i < n; i++)
-			memset(R + i * n, 0, n * sizeof(T));
-
-		for (j = 0; j < n; j++)
-		{
-			for (m = 0; m < n; m++)
-			{
-				for (k = 0; k < n; k++)
-				{
-					R[j * n + k] += Q[j * n + m] * REF_A[m * j + k];
-				}
-			}
-		}*/
-
+	void R_recount(size_t i_start)
+	{
+#pragma omp parallel for private (i) //512/64 no result
 		for (i = 0; i < n; i++)
 			copy(R + i * n, R + (i + 1) * n, R_tmp + i * n);
 
+		for (i = i_start; i < n; i++)
+			memset(R + i * n + i_start + block_size, 0, (n - i_start - block_size) * sizeof(T));
+
+#pragma omp parallel for simd private(k,j) //512/64 obvious boost WHY IS IT CORRECT
+		for (k = i_start + block_size; k < n; k++)
+			for (j = i_start; j < n; j++)
+				for (m = 0; m < n; m++)
+
+					R[j * n + k] += Q[j * n + m] * R_tmp[m * n + k];
+	}
+
+	void Q_count(size_t i_start)
+	{
+		memset(Q, 0, n * n * sizeof(T));
+
+#pragma omp parallel for simd private(k,j,m) //512/64 slowdown
 		for (j = i_start; j < n; j++)
+			for (k = i_start; k < n; k++)
+				for (m = i_start; m < i_start + min((j + 1 - i_start), block_size); m++)
+
+					Q[j * n + k] += R[(j + 1) * n + m] * w[k * block_size + (m - i_start)];
+
+		for (j = 0; j < n; j++)
+			Q[j * n + j] += 1;
+	}
+
+	void W_count(size_t i_start)
+	{
+		memset(w, 0, n * block_size * sizeof(T));
+
+		norm = 0.0;
+
+		for (i = i_start; i < n; i++)
+			norm += R[(i + 1) * n + i_start] * R[(i + 1) * n + i_start];
+
+		norm = -2 / (norm);
+
+		for (i = i_start; i < n; i++)
+			w[i * block_size] = (norm)*R[(i + 1) * n + i_start];
+
+		for (i = 1; i < block_size; i++)
 		{
-			for (m = 0; m < n; m++)
-			{
-				for (k = i_start + b_size; k < n; k++)
-				{
-					R[j * n + k] += R_tmp[j * n + m] * Q[m * n + k];
-				}
-			}
+
+			norm = 0.0;
+
+			for (k = i + i_start; k < n; k++)
+				norm += R[(k + 1) * n + (i + i_start)] * R[(k + 1) * n + (i + i_start)];
+
+			norm = -2 / (norm);
+
+#pragma omp parallel for /*simd*/ private(j,k,m) //512/64 MULTIPLE boost WHY IS IT CORRECT AND FAST WITHOUT SIMD
+			for (j = 0; j < n; j++)
+				for (k = i + i_start; k < n; k++)
+					for (m = 0; m < i; m++)
+
+						w[j * block_size + i] += w[j * block_size + m] * R[(k + 1) * n + m + i_start] * R[(k + 1) * n + i + i_start] * (norm);
+
+			for (j = i + i_start; j < n; j++)
+				w[j * block_size + i] += R[(j + 1) * n + i + i_start] * (norm);
 		}
+
 	}
 
 	void HHolder_Q()
 	{
-		//#pragma omp parallel for private(i)
+		//#pragma omp parallel for private(i)	//512/64, 1024/64 no result
 		for (i = 0; i < n; i++)
 			copy(REF_A + i * n, REF_A + (i + 1) * n, Q + i * n);
 
 		for (i = 0; i < n; i++)
 		{
-			//#pragma omp parallel for private(j)
+			#pragma omp parallel for private(j) //512/64 no result, 1024/64 small boost
 			for (j = 0; j < n; j++)
 				Q[j * n + i] /= R[i * n + i];
 
-			copy(R + i * (n + 1) + 1, R + (i + 1) * n, factor);
+			copy(R + i * (n + 1) + 1, R + (i + 1) * n, R_tmp);
 
-			//#pragma omp parallel for private(k)
+			#pragma omp parallel for private(k) //512/64 obvious boost
 			for (k = 0; k < n; k++)
 				for (j = i + 1; j < n; j++)
 
-					Q[k * n + j] -= factor[j - (i + 1)] * Q[k * n + i];
+					Q[k * n + j] -= R_tmp[j - (i + 1)] * Q[k * n + i];
 
 		}
 	}
 
-
 	bool check()
 	{
-		T* tmp = new T[n * n]();
-
-		for (i = 0; i < n; i++)
-			for (k = 0; k < n; k++)
-				for (j = 0; j < n; j++)
-					tmp[i * n + j] += Q[i * n + k] * R[k * n + j];
+		memset(R_tmp, 0, n * n * sizeof(T));
 
 		for (i = 0; i < n; i++)
 			for (j = 0; j < n; j++)
-				if (abs(REF_A[i * n + j] - tmp[i * n + j]) >= eps)
+				for (k = 0; k <= j; k++)
+					R_tmp[i * n + j] += Q[i * n + k] * R[k * n + j];
+
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+				if (abs(REF_A[i * n + j] - R_tmp[i * n + j]) >= eps)
 					return false;
+
+		memset(R_tmp, 0, n * n * sizeof(T));
+
+		for (i = 0; i < n; i++)										//is ortogonal?
+			for (k = 0; k < n; k++)
+				for (j = 0; j < n; j++)
+					R_tmp[i * n + j] += Q[i * n + k] * Q[j * n + k];
+
+		for (i = 0; i < n; i++) 
+		{
+			for (k = 0; k < i; k++)
+				if(abs(R_tmp[i * n + k])>=eps)
+					return false;
+
+			if (abs(R_tmp[i * n + i] - 1) >= eps)
+				return false;
+
+				for (k = i+1; k < n; k++)
+					if (abs(R_tmp[i * n + k]) >= eps)
+						return false;
+
+		}
 
 		return true;
 	}
@@ -287,7 +343,7 @@ public:
 
 			}
 
-		else if (s == 'Q')
+		else  if (s == 'Q')
 
 			for (i = 0; i < n; i++)
 			{
@@ -313,12 +369,13 @@ public:
 	{
 		delete[]Q;
 		delete[]R;
-		delete[]v;
 		delete[]u;
 		delete[]REF_A;
-		delete[]factor;
+		delete[]factor_block;
+		delete[]w;
+		delete[]R_tmp;
 
-		Q = R = u = REF_A = v = factor = nullptr;
-		i = j = k = 0;
+		Q = R = u = REF_A = factor_block = w = R_tmp = nullptr;
+		i = j = k = m = 0;
 	}
 };
--- QR-decomposition/QR_main.cpp
+++ QR-decomposition/QR_main.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <cstdlib>
 #include <omp.h>
 #include "QR_header.h"
 
 int main()
 {
-	//int count;
+	int count;
 	size_t n, block_size;
 	cout << "Enter matrix dimentions: " << endl;
-	cin >> n;
-	/*while (1)
-	{*/
+	//cin >> n;
+	//while (1)
+	//{
+	n = 1024;
 		cout << "Enter block size: ";
-		cin >> block_size;
-		//count = 0;
+		block_size = 64;
+		//cin >> block_size;
+		count = 0;
 		/*while (count < 5)
 		{*/
 
-			QR <double> t(1, n, block_size);
-			/*auto start_A{ chrono::steady_clock::now() };*/
+			QR <double> t(0, n, block_size);
+			auto start_A{ chrono::steady_clock::now() };
 
 			t.HHolder_R();
 
-			t.out('R');
+			//t.out('R');
 
-			/*auto end_A{ chrono::steady_clock::now() };
+			auto end_A{ chrono::steady_clock::now() };
 			chrono::duration<double> elapsed_seconds_A = end_A - start_A;
-			auto start_Q = chrono::steady_clock::now();*/
+			auto start_Q = chrono::steady_clock::now();
 
 			t.HHolder_Q();
 
-			t.out('Q');
+			//t.out('Q');
 
-			/*auto end_Q = chrono::steady_clock::now();
+			auto end_Q = chrono::steady_clock::now();
 			chrono::duration<double>  elapsed_seconds_Q = end_Q - start_Q;
 			cout << "Time spent: " << elapsed_seconds_A.count() << " sec + " << elapsed_seconds_Q.count() << " sec = ";
 			elapsed_seconds_A += elapsed_seconds_Q;
 			cout << elapsed_seconds_A.count() << endl;
-			count++;*/
+			count++;
 
 			if (t.check())
 
@@ -45,18 +45,6 @@ int main()
 			else cout << "QR-decomposition is incorrect";
 
 			cout << endl;
-		/*}*/
+		//}
 	//}
-	/*t.HHolder_Q();
-	end = chrono::steady_clock::now();
-	elapsed_seconds = end - start;
-	cout << "Time spent: " << elapsed_seconds.count() << " sec" << endl;
-	if (t.check())
-		cout << "QR-decomposition is correct";
-	else cout << "QR-decomposition is incorrect";
-	cout << endl;*/
-	/*QR <double> t(0, n, block_size);
-	t.out('R');*/
-
-	return 0;
 }
